474de020800104b5cf7312db8b1de4ff84146594
Zeeshan Meghji <zeeshan_meghji@hotmail.com>
2019-10-06 19:14:50 -0400

    completed module 4

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,24 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
 
 import { IProduct } from './product';
 import { ProductService } from './product.service';
+import { NgModel } from '@angular/forms';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, AfterViewInit {
+    ngAfterViewInit(): void {
+        // this.filterElementRef.nativeElement.focus();
+    }
+
     pageTitle: string = 'Product List';
     private _listFilter : string;
+    private _sub : Subscription;
     get listFilter(): string{
         return this._listFilter;
     }
     set listFilter(value: string){
         this._listFilter = value;
+        if (!this._sub){
+        this._sub= this.filterInput.valueChanges.subscribe( x=>{
+            this.performFilter(x);
+        })
+    }
+        
+
+        
+        this.filterElementRef.nativeElement.focus();
+
     }
     // listFilter: string;
     showImage: boolean;
-
+    @ViewChild('filterElement') filterElementRef : ElementRef;
+    // @ViewChild(NgModel) filterInput: NgModel;
+    private _filterInput: NgModel;
+    get filterInput(){
+        return this._filterInput;
+    }
+    @ViewChild(NgModel)
+    set filterInput(input: NgModel ){
+        this._filterInput = input;
+    }
     imageWidth: number = 50;
     imageMargin: number = 2;
     errorMessage: string;
@@ -36,6 +62,10 @@ export class ProductListComponent implements OnInit {
             },
             (error: any) => this.errorMessage = <any>error
         );
+
+        // this.filterInput.valueChanges.subscribe( x=>{
+        //     this.performFilter(x);
+        // })
     }
     onFilterChange(filter: string){
         this.listFilter = filter;
